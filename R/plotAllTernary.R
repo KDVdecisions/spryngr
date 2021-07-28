0cb5832a3956a2fd312695d1af0cd4f8ffadfd11
edixon1 <eliot.dixon@kdv-decisions.com>
2021-07-28 14:14:17 -0400

    implemented rough version of ternary plotting with new data cleaning system.  Next step is to revise, clean up, and then rework violin plotting system to use same functions

--- R/plotAllTernary.R
+++ R/plotAllTernary.R
@@ -6,6 +6,10 @@ plotAllTernary <- function(outputFolder, showMean = TRUE, palette = NULL,
                            marblesAsDiscrete = FALSE, subsetIds = NULL) {
   #read in outline file
   outline <- read_excel(paste0(outputFolder, "/Data/outline.xlsx"))
+  #Convert comma separated values to lists of vectors
+  outline$CONTINUOUS_LABELS <- toList(outline$CONTINUOUS_LABELS)
+  outline$DISCRETE_LABELS <- toList(outline$DISCRETE_LABELS)
+  outline$LEVELS <- toList(outline$LEVELS)
 
 
   #set which classes should be treated as discrete variables
@@ -21,41 +25,107 @@ plotAllTernary <- function(outputFolder, showMean = TRUE, palette = NULL,
     subsetIds <- outline$ID
   }
 
-  discreteData <- getDiscreteTables(outputFolder, outline, subsetIds, discreteClasses)
-  continuousData <- getContinuousTables(outputFolder, outline, subsetIds, continuousClasses)
-
-  print(discreteData)
-
-#
-#   #initialize fill container
-#   discreteFills <- list()
-#   #initialize label pair container
-#   discreteLabPairs <- list()
-#
-#   #Iterate over discrete data, format data for each signifier, generate fills for
-#   #each level
-#   for(i in 1:length(discreteData)){
-#     #Format discrete data
-#     discreteData[[i]] <- formatDiscrete(discreteData[[i]]$SET, discreteOutline[i,])
-#     #Generate a set of fills for each discrete question
-#     nLevels <- length(levels(discreteData[[i]]$SET))
-#     discreteFills[[i]] <- colorRampPalette(brewer.pal(8, palette))(nLevels)
-#     #Get list of labels to use for each discrete level within plots
-#     discreteLabPairs[[i]] <- generateDiscreteLabels(discreteOutline[i,])
-#   }
+  #read in discrete data
+  discreteData <- getDiscreteData(outputFolder, outline, subsetIds, discreteClasses)
+  #format discrete data
+  discreteData <- formatDiscrete_new(discreteData, outline)
+  #generate key-value structure where keys are discrete levels in data and values are
+  #labels to be used within graph
+  discreteLabelPairs <- apply(outline[outline$ID %in% names(discreteData),],
+                              MARGIN = 1, FUN = generateDiscreteLabels)
+  #generate list of fills to be used for each discrete variable
+  discreteFills <- lapply(discreteData, function(x){
+    nLevels <- length(levels(x$observation))
+    return(colorRampPalette(brewer.pal(8, palette))(nLevels))
+  })
 
 
+  continuousData <- getContinuousData(outputFolder, outline, subsetIds, continuousClasses)
+
+  plots <- list()
+
+  plotIndex <- 1
+  for(i in 1:length(continuousData)){
+    thisContinuousOutline <- outline[outline$ID == names(continuousData)[i],]
+    thisContinuousData <- continuousData[[i]]
+    continuousLabels <- unlist(thisContinuousOutline$CONTINUOUS_LABELS)
+
+    for(j in 1:length(discreteData)){
+      thisDiscreteData <- discreteData[[j]]
+      discreteLabel = outline[outline$ID == names(discreteData)[j],]$QUESTION
+      p <- ternaryPoint(continuous = thisContinuousData[thisDiscreteData$rowId,],
+                       discrete = thisDiscreteData$observation,
+                       fill = discreteFills[[j]],
+                       labelPairs = discreteLabelPairs[[j]],
+                       title = thisContinuousOutline$QUESTION,
+                       discreteTitle = discreteLabel,
+                       lLabel = continuousLabels[1],
+                       tLabel = continuousLabels[2],
+                       rLabel = continuousLabels[3],
+                       showMean = showMean)
+
+
+      plots[[plotIndex]] <- p
+      plotIndex = plotIndex + 1
+
+    }
+  }
+
+  return(plots)
 }
 
 
+#'function  converts discrete data to type factor and reformats discrete so each
+#'observation only contains a single factor level and adds a rowId field so each
+#'factor level can be associated with corresponding observations
+#'@param discreteData: list containing each discrete data.frame
+#'@param outline: dataframe  containing all meta data
+#'
+formatDiscrete_new <- function(discreteData, outline){
+  for(i in 1:length(discreteData)){
+    #flatten data so each row has a single factor level, add a rowId field so each
+    #level can be associated with original observation
+    discreteData[[i]] <- flattenDiscrete(discreteData[[i]]$SET)
 
-#' read in all discrete data sheets and return as list of tibbles
+    #store the meta data from outline file for this discrete question
+    thisOutline <- outline[outline$ID == names(discreteData)[i],]
+
+    #if user has specified this as an ordered factor convert to ordered factor
+    #otherwise convert to unordered factor
+    if(thisOutline$ORDERED){
+
+      discreteData[[i]]$observation <- factor(x = discreteData[[i]]$observation, levels = unlist(thisOutline$LEVELS),
+                                         ordered = TRUE)
+    } else {
+      discreteData[[i]]$observation <- factor(x = discreteData[[i]]$observation)
+    }
+  }
+  return(discreteData)
+}
+
+#reformats discrete so eachobservation only contains a single factor level and adds a rowId field so each
+#'@param discreteSet: vector containing all present levels within a discrete data observation
+#'
+flattenDiscrete <- function(discreteSet){
+  observation <- c()
+  rowId <- c()
+  for(i in 1:NROW(discreteSet)){
+    for(ob in unlist(discreteSet[i])){
+      observation <- c(observation, ob)
+      rowId <- c(rowId, i)
+    }
+  }
+  return(data.frame(observation, rowId))
+}
+
+
+#' read in all discrete data sheets and return as list of dataframes
 #' @param outputFolder: path to folder containing outline and datasheets
 #' @param outline: outline data sheet containing all question meta data
 #' @param subsetIds: character vector of ids specifying subset of questions to read in
 #' @param discreteClasses: character vector of which classes may be treated as discrete variables
 #'
-getDiscreteTables <- function(outputFolder, outline, subsetIds, discreteClasses){
+getDiscreteData <- function(outputFolder, outline, subsetIds, discreteClasses){
   #store discrete data file path to note have as to not repeatedly call paste0
   discreteDataPath <- paste0(outputFolder, "/Data/discreteData.xlsx")
 
@@ -95,7 +165,7 @@ getDiscreteTables <- function(outputFolder, outline, subsetIds, discreteClasses)
 #' @param subsetIds: character vector of ids specifying subset of questions to read in
 #' @param continuousClasses: character vector of which classes may be read in as continuous variables
 #'
-getContinuousTables <- function(outputFolder, outline, subsetIds, continuousClasses){
+getContinuousData <- function(outputFolder, outline, subsetIds, continuousClasses){
   #store continuous data file path to note have as to not repeatedly call paste0
   continuousDataPath <- paste0(outputFolder, "/Data/continuousData.xlsx")
 
@@ -115,15 +185,11 @@ getContinuousTables <- function(outputFolder, outline, subsetIds, continuousClas
     sheetId <- str_split(sheetName, " ")[[1]][1]
     if(sheetId %in% continuousIds){
       continuousTables[[sheetId]] <- read_excel(continuousDataPath,
-                                                sheetName)
+                                                sheetName) %>%
+        select(`L`, `T`, `R`)
     }
   }
 
   return(continuousTables)
 }
-
-
-
-
-
 
--- R/ternaryPlot.R
+++ R/ternaryPlot.R
@@ -10,33 +10,37 @@
 #'@param showMean:
 #'
 ternaryPoint <- function(continuous, discrete = NULL, fill = NULL, labelPairs = NULL,
-                        title = "Title",lLabel = "Left Label", tLabel = "Top Label",
-                        rLabel = "Right Label", showMean = TRUE){
+                        title = "Title", discreteTitle = "Discrete", lLabel = "Left Label",
+                        tLabel = "Top Label", rLabel = "Right Label", showMean = TRUE){
 
   #if no discrete variable is provided, just call all levels responses
   if(is.null(discrete)){
     discrete <- rep("responses", NROW(continuous))
   }
 
-  plotData <- data.frame(continuous, discrete)
+  plotData <- data.frame(continuous, discrete) %>%
+    na.omit()
 
   #find levels which are present within the data (i.e. not dropped due to NA)
   presentLevels <- factor(plotData$discrete) %>%
     levels()
+
   #assign y axis tick marks
-  yAxisTicks <- unlist(labelPairs[presentLevels])
+  #yAxisTicks <- unlist(labelPairs[presentLevels])
+
+  print(labelPairs)
 
   p <- ggtern(data = plotData) +
-    geom_point(mapping = aes(x = l, y = t, z = r, fill = discrete)) +
-    Llab(lLabel) +
-    Tlab(tLabel) +
-    Rlab(rLabel) +
-    labs(title = title) +
+    geom_point(mapping = aes(x = `L`, y = `T`, z = `R`, color = discrete)) +
+    Llab(str_wrap(lLabel, width = 7)) +
+    Tlab(str_wrap(tLabel, width = 7)) +
+    Rlab(str_wrap(rLabel, width = 7)) +
+    labs(title = title, color = str_wrap(discreteTitle, width = 25)) +
     theme(plot.title = element_text(hjust = 0.5)) +
-    scale_fill_manual(values = fill, breaks = levels(plotData$discrete))
-
+    scale_color_manual(values = fill, labels = labelPairs)
+  #labels = unlist(labelPairs[presentLevels])
   if(showMean){
-    p <- p + geom_mean_ellipse(mapping = aes(l,r,t))
+    p <- p + geom_mean_ellipse(mapping = aes(x = `L`, y = `T`, z = `R`))
   }
 
   return(p)
